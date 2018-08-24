333217ef07290e7b71d009bc0946dcd84fc47c27
tomschweers <trschweers@gmail.com>
2018-08-24 09:44:30 -0700

    deploying for heroku

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to my life</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          We are still in development...
         </p>
       </div>
     );
